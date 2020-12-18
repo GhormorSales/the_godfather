76eb4efd466f2f2d42b12681ddfa06e82fe5ea69
David Saksa <david@Davids-MacBook-Air.local>
2020-12-18 12:04:49 -0500

    Update to phone number

--- src/app/views/landing/landing.component.ts
+++ src/app/views/landing/landing.component.ts
@@ -13,7 +13,7 @@ export class LandingComponent implements OnInit {
   public checkInput(event) {
     const input = (<HTMLInputElement>document.getElementById("fullName")).value;
     if (input.toLowerCase().replace(" ", "") === "logandaft") {
-      alert("You are correct. The LAST NAME is of particular importance. Add the number '6' at the end, and you have the password!");
+      alert("Congratulations! You figured out who the CEO is! To crack the password, now use his last name, the number 6, and the company name together (capitals on first letters of name and company). As a reminder the email is daftlogan@gmail.com.");
     } else {
       alert("You are wrong, try again! Make sure you are looking at all of the links!");
     }
