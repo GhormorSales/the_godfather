b9d82ed2fc8515a2cbb9c34d5fba5a0d3fb92934
David Saksa <david@Davids-MacBook-Air.local>
2020-12-20 16:40:20 -0500

    Final push pre-release

--- src/app/views/landing/landing.component.ts
+++ src/app/views/landing/landing.component.ts
@@ -13,7 +13,7 @@ export class LandingComponent implements OnInit {
   public checkInput(event) {
     const input = (<HTMLInputElement>document.getElementById("fullName")).value;
     if (input.toLowerCase().replace(" ", "") === "logandaft") {
-      alert("Congratulations! You figured out who the CEO is! The password is Daft6Rentax. As a reminder the email is daftlogan@gmail.com.");
+      alert("Congratulations! You figured out who the CEO is! The password is Daft6Rentax. The email address is in the format last name first name @gmail.com.");
     } else {
       alert("You are wrong, try again! Make sure you are looking at all of the links!");
     }
